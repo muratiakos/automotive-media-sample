43f916847a3f4173af50ce57ba1cc9c84e665d43
Murati Akos <akos@murati.hu>
2026-08-25 05:51:23 +0200

    KTS migrations

---
+++ app/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "com.volvocars.otaevent"
+        minSdk = 29
+        targetSdk = 37
+        versionCode = 5
+        versionName = "0.5"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    namespace = "com.volvocars.mediasample"
+}
+
+val kotlin_version: String by rootProject.extra
+val glide_version: String by rootProject.extra
+val exoplayer_version: String by rootProject.extra
+val koin_version: String by rootProject.extra
+val junit_version: String by rootProject.extra
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+
+    implementation("androidx.core:core-ktx:1.19.0")
+    implementation("androidx.appcompat:appcompat:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.2")
+    implementation("androidx.media:media:1.8.0")
+    implementation("com.github.bumptech.glide:glide:$glide_version")
+    //kapt("com.github.bumptech.glide:compiler:$glide_version")
+
+    // Exo player
+    api("com.google.android.exoplayer:exoplayer-core:$exoplayer_version")
+    api("com.google.android.exoplayer:exoplayer-ui:$exoplayer_version")
+    api("com.google.android.exoplayer:extension-mediasession:$exoplayer_version")
+
+    // DI
+    implementation("io.insert-koin:koin-core:$koin_version")
+    implementation("io.insert-koin:koin-android:4.2.2")
+
+    testImplementation("junit:junit:$junit_version")
+    testImplementation("androidx.test:core:1.7.0")
+    testImplementation("io.insert-koin:koin-test:$koin_version")
+}
+
+repositories {
+    mavenCentral()
+}
